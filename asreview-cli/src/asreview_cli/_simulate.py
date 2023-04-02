9a66bdb31c356731c7bcad5639144d075dd97b94
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-04-02 21:17:16 +0200

    added option --write_interval to simulate cli

--- asreview-cli/src/asreview_cli/_simulate.py
+++ asreview-cli/src/asreview_cli/_simulate.py
@@ -4,8 +4,9 @@ import click
 @click.command("simulate", help="Run simulation and exit; terminates parsing\n\n    DATA_FILE: " +
                "path to the fully labeled data")
 @click.argument("data_file", type=click.Path(exists=True, readable=True))
+@click.option("--write-interval", "write_interval", type=click.INT, help="Write interval.")
 @click.pass_obj
-def simulate(obj, data_file):
+def simulate(obj, data_file, write_interval):
     click.echo("simulation starting")
     click.echo("doing things...")
     click.echo("simulation ended")
