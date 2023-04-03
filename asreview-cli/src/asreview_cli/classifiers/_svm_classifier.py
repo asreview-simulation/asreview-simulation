634e1866593df41d9ecfa5c28835458f0a6607e8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-04-03 19:08:46 +0200

    typo

--- asreview-cli/src/asreview_cli/classifiers/_svm_classifier.py
+++ asreview-cli/src/asreview_cli/classifiers/_svm_classifier.py
@@ -6,7 +6,7 @@ from .._epilog import epilog
 name = SvmClassifier.name
 
 
-@click.command(name=f"c-{name}", help="Use Support Vecor Machine classifier", epilog=epilog)
+@click.command(name=f"c-{name}", help="Use Support Vector Machine classifier", epilog=epilog)
 @click.option("--gamma", "gamma", default="auto", type=click.STRING, help="hyperparameter 'auto'.")
 @click.option("--class_weight", "class_weight", default=0.249, type=click.FLOAT, help="hyperparameter 'class_weight'.")
 @click.option("--c", "c", default=15.4, type=click.FLOAT, help="hyperparameter 'c'.")
