639d9929e23976eb23d507fbe32c386639fb1011
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-04-04 16:02:54 +0200

    small text change to epilog

--- asreview-cli/src/asreview_cli/_epilog.py
+++ asreview-cli/src/asreview_cli/_epilog.py
@@ -2,5 +2,5 @@ epilog = """
 This command is chainable with other commands. Chained commands
 are evaluated left to right; make sure to end the chain with
 either a 'start' command or a 'print-settings' command, otherwise
-it appears like nothing is happening.
+it may appear like nothing is happening.
 """
