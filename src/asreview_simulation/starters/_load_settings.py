977afbd28817926f0c1f1ccbf7271f11bb3fb989
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-05-24 13:38:35 +0200

    added loading of models configuration from a settings file

--- src/asreview_simulation/starters/_load_settings.py
+++ src/asreview_simulation/starters/_load_settings.py
@@ -1,3 +1,5 @@
+import json
+
 import click
 
 
@@ -11,10 +13,42 @@ def load_settings(obj, settings_file):
     assert obj.provided.extractor is False, "Attempted reassignment of extractor"
     assert obj.provided.querier is False, "Attempted reassignment of querier"
 
-    # TODO load config, assign state
-    click.echo(f"Would load configuration from file {settings_file.name}")
+    click.echo(f"Loading configuration from file '{settings_file.name}'...")
+    with open(settings_file.name) as fid:
+        settings = json.load(fid)
 
-    obj.provided.balancer = True
-    obj.provided.classifier = True
-    obj.provided.extractor = True
-    obj.provided.querier = True
+    if "balancer" in settings.keys():
+        try:
+            obj.balancer.model = settings["balancer"]["model"]
+            obj.balancer.params = settings["balancer"]["params"]
+            obj.provided.balancer = True
+        except KeyError as e:
+            print("Expected balancer settings to include both a model and its parameterization.")
+            raise e
+
+    if "classifier" in settings.keys():
+        try:
+            obj.classifier.model = settings["classifier"]["model"]
+            obj.classifier.params = settings["classifier"]["params"]
+            obj.provided.classifier = True
+        except KeyError as e:
+            print("Expected classifier settings to include both a model and its parameterization.")
+            raise e
+
+    if "extractor" in settings.keys():
+        try:
+            obj.extractor.model = settings["extractor"]["model"]
+            obj.extractor.params = settings["extractor"]["params"]
+            obj.provided.extractor = True
+        except KeyError as e:
+            print("Expected extractor settings to include both a model and its parameterization.")
+            raise e
+
+    if "querier" in settings.keys():
+        try:
+            obj.querier.model = settings["querier"]["model"]
+            obj.querier.params = settings["querier"]["params"]
+            obj.provided.querier = True
+        except KeyError as e:
+            print("Expected querier settings to include both a model and its parameterization.")
+            raise e
