1d6974264c13327f64d70f982f412bce75f9964e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-05-31 15:47:40 +0200

    removed duplicated code

--- src/asreview_simulation/classifiers/__init__.py
+++ src/asreview_simulation/classifiers/__init__.py
@@ -25,10 +25,3 @@ __all__ = [
     "nn_2_layer_classifier",
     "svm_classifier"
 ]
-
-for _item in dir():
-    if not _item.endswith('__'):
-        assert _item in __all__, f"Named export {_item} missing from __all__ in {__package__}"
-for _item in __all__:
-    assert _item in dir(), f"__all__ includes unknown item {_item} in {__package__}"
-del _item
--- src/asreview_simulation/samplers/__init__.py
+++ src/asreview_simulation/samplers/__init__.py
@@ -9,10 +9,3 @@ __all__ = [
     "random_prior_sampler",
     "handpicked_prior_sampler"
 ]
-
-for _item in dir():
-    if not _item.endswith('__'):
-        assert _item in __all__, f"Named export {_item} missing from __all__ in {__package__}"
-for _item in __all__:
-    assert _item in dir(), f"__all__ includes unknown item {_item} in {__package__}"
-del _item
