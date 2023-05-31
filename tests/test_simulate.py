871b746e20a00cd1fda3c525ca19246edb993dcd
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-05-31 15:49:21 +0200

    added asserts for settings file, data file, project file

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,32 +1,85 @@
 import os
+import hashlib
+import zipfile
+import json
 from tempfile import TemporaryDirectory
+from pathlib import Path
 from asreview.entry_points import SimulateEntryPoint
 from click.testing import CliRunner
 from asreview_simulation import cli
 
 
 def test_simulate_start():
+    def calc_hash(filename):
+        with open(filename, "rb") as f:
+            contents = f.read()
+            return hashlib.sha256(contents).hexdigest()
+
+    def compare_project_json():
+        with open(p1 / "project.json", "r") as f:
+            expected = json.load(f)
+        with open(p2 / "project.json", "r") as f:
+            actual = json.load(f)
+
+        assert actual["version"] == expected["version"]
+        assert actual["mode"] == expected["mode"]
+        assert actual["dataset_path"] == expected["dataset_path"]
+        assert actual["feature_matrices"][0]["id"] == expected["feature_matrices"][0]["id"]
+        assert actual["feature_matrices"][0]["filename"] == expected["feature_matrices"][0]["filename"]
+        assert actual["reviews"][0]["status"] == expected["reviews"][0]["status"]
+
+    def compare_data():
+        fname = f"{dataset.split(':')[1]}.csv"
+        assert calc_hash(p1 / "data" / fname) == calc_hash(p2 / "data" / fname)
+
+    def compare_settings_metadata_json():
+        settings1 = p1 / "reviews" / get_review_id(p1) / "settings_metadata.json"
+        settings2 = p2 / "reviews" / get_review_id(p2) / "settings_metadata.json"
+        assert calc_hash(settings1) == calc_hash(settings2)
+
+    def get_review_id(p):
+        with open(p / "project.json", "r") as f:
+            project_data = json.load(f)
+        return project_data["reviews"][0]["id"]
+
     def run_asreview_simulate_cli():
         args = [
             dataset,
-            "--state_file", os.path.join(d, "simulate.asreview")
+            "--state_file", str(p1)
         ]
         SimulateEntryPoint().execute(args)
+        # unzip simulate results
+        src = p1.parent / Path(p1.name + ".tmp")
+        tgt = p1
+        os.rename(tgt, src)
+        with zipfile.ZipFile(src, "r") as f:
+            f.extractall(tgt)
+        os.remove(src)
 
     def run_asreview_simulation_start_cli():
         runner = CliRunner()
         args = [
             "start",
             "--dataset", dataset,
-            os.path.join(d, "simulation.asreview")
+            str(p2)
         ]
         result = runner.invoke(cli, args)
         assert result.exit_code == 0
+        # rename simulation results
+        src = p2.parent / Path(p2.name + ".tmp")
+        tgt = p2
+        os.rename(src, tgt)
 
     dataset = "benchmark:van_de_Schoot_2017"
-    with TemporaryDirectory(prefix="pytest.") as d:
+    with TemporaryDirectory(prefix="pytest.") as tmpdir:
+        p1 = Path(tmpdir) / "simulate.asreview"
+        p2 = Path(tmpdir) / "simulation.asreview"
         run_asreview_simulate_cli()
         run_asreview_simulation_start_cli()
-        print()
 
-    # compare the two results
+        # compare the two results
+        compare_project_json()
+        compare_data()
+        compare_settings_metadata_json()
+
+        print()
