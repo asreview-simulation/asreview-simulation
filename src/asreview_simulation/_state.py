4a8da79d97f7feae652e1a18797af872e76fc54a
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-05 16:34:30 +0200

    updated state implementation to avoid problems with class variables v instance variables

--- src/asreview_simulation/_state.py
+++ src/asreview_simulation/_state.py
@@ -3,52 +3,83 @@ from dataclasses import dataclass
 
 @dataclass
 class Model:
-    model: str
-    params: dict
+    def __init__(self, model: str, params: dict):
+        self.model = model
+        self.params = params
+
+    def __repr__(self):
+        return f"Model(model='{self.model}', params={self.params}"
 
 
 @dataclass
 class Provided:
-    balancer: bool = False
-    classifier: bool = False
-    extractor: bool = False
-    querier: bool = False
-    sampler: bool = False
+    def __init__(self, balancer: bool = False, classifier: bool = False, extractor: bool = False,
+                 querier: bool = False, sampler: bool = False):
+        self.balancer = balancer
+        self.classifier = classifier
+        self.extractor = extractor
+        self.querier = querier
+        self.sampler = sampler
 
 
 @dataclass
 class State:
-    provided: Provided = Provided()
-    balancer: Model = Model(
-        "double",
-        {
-            "a": 2.155,
-            "alpha": 0.94,
-            "b": 0.789,
-            "beta": 1.0,
-        },
-    )
-    classifier: Model = Model(
-        "nb",
-        {
-            "alpha": 3.822,
-        },
-    )
-    extractor: Model = Model(
-        "tfidf",
-        {
-            "ngram_max": 1,
-            "stop_words": "english",
-        },
-    )
-    sampler: Model = Model(
-        "random",
-        {
-            "n_excluded": 1,
-            "n_included": 1,
-        },
-    )
-    querier: Model = Model(
-        "max",
-        {},
-    )
+    def __init__(self, provided=None, balancer=None, classifier=None, extractor=None, sampler=None, querier=None):
+        if provided is not None:
+            self.provided = provided
+        else:
+            self.provided = Provided()
+
+        if balancer is not None:
+            self.balancer = balancer
+        else:
+            self.balancer = Model(
+                "double",
+                {
+                    "a": 2.155,
+                    "alpha": 0.94,
+                    "b": 0.789,
+                    "beta": 1.0,
+                },
+            )
+
+        if classifier is not None:
+            self.classifier = classifier
+        else:
+            self.classifier = Model(
+                "nb",
+                {
+                    "alpha": 3.822,
+                },
+            )
+
+        if extractor is not None:
+            self.extractor = extractor
+        else:
+            self.extractor = Model(
+                "tfidf",
+                {
+                    "ngram_max": 1,
+                    "stop_words": "english",
+                },
+            )
+
+        if sampler is not None:
+            self.sampler = sampler
+        else:
+            self.sampler = Model(
+                "random",
+                {
+                    "init_seed": None,
+                    "n_excluded": 1,
+                    "n_included": 1,
+                },
+            )
+
+        if querier is not None:
+            self.querier = querier
+        else:
+            self.querier = Model(
+                "max",
+                {},
+            )
