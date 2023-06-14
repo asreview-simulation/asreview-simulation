155afd66d73dca2d8fe867860c83c945de9fb7e1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-14 15:29:42 +0200

    added test for pretty printed settings

---
+++ tests/it/test_print_settings.py
@@ -0,0 +1,55 @@
+from click.testing import CliRunner
+from asreview_simulation.cli import cli
+
+
+def test_with_default_settings():
+    runner = CliRunner()
+    args = [
+        "print-settings",
+        "--pretty"
+    ]
+    result = runner.invoke(cli, args)
+    assert result.exit_code == 0
+    assert result.output == """{
+    "balancer": {
+        "abbr": "double",
+        "params": {
+            "a": 2.155,
+            "alpha": 0.94,
+            "b": 0.789,
+            "beta": 1.0
+        }
+    },
+    "classifier": {
+        "abbr": "nb",
+        "params": {
+            "alpha": 3.822
+        }
+    },
+    "extractor": {
+        "abbr": "tfidf",
+        "params": {
+            "ngram_max": 1,
+            "stop_words": "english"
+        }
+    },
+    "querier": {
+        "abbr": "max",
+        "params": {
+            "n_instances": 1
+        }
+    },
+    "sampler": {
+        "abbr": "random",
+        "params": {
+            "init_seed": null,
+            "n_excluded": 1,
+            "n_included": 1
+        }
+    },
+    "stopping": {
+        "abbr": "min",
+        "params": {}
+    }
+}
+"""
