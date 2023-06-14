2678a908a33bb9420e8c92c883b8729bb2ebc7fa
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-14 08:16:58 +0200

    removed triple balancer;m refs #28

--- src/asreview_simulation/cli/balancers/__init__.py
+++ src/asreview_simulation/cli/balancers/__init__.py
@@ -1,17 +1,14 @@
 from ._double_balancer import double_balancer
 from ._simple_balancer import simple_balancer
-from ._triple_balancer import triple_balancer
 from ._undersample_balancer import undersample_balancer
 
 
 del _double_balancer
 del _simple_balancer
-del _triple_balancer
 del _undersample_balancer
 
 __all__ = [
     "double_balancer",
     "simple_balancer",
-    "triple_balancer",
     "undersample_balancer",
 ]
