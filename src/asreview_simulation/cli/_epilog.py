12f13633d0f5ae6ce1a404dedb5ddab3ec8b4389
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-20 12:43:32 +0200

    updated help epilog

--- src/asreview_simulation/cli/_epilog.py
+++ src/asreview_simulation/cli/_epilog.py
@@ -1,6 +1,9 @@
 epilog = """
 This command is chainable with other commands. Chained commands
 are evaluated left to right; make sure to end the chain with
-either a 'start' command or a 'print-settings' command, otherwise
-it may appear like nothing is happening.
+the 'start' command, otherwise it may appear like nothing is happening.
+
+Please report any issues at:
+
+https://github.com/asreview-tuning/asreview-simulation/issues.
 """
