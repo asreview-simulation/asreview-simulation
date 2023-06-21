c1f7a7e4f40a1fb88d3001f18234a416ed2a57fc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-06-21 11:17:10 +0200

    updated data comparison function to be able to handle --data as well as --dataset

--- tests/helpers/_compare_data_csv.py
+++ tests/helpers/_compare_data_csv.py
@@ -1,8 +1,16 @@
 from ._calc_hash import calc_hash
+from pathlib import Path
 
 
-def compare_data_csv(p1, p2, dataset):
-    fname = (
-        f"{dataset[10:]}.csv" if dataset.startswith("benchmark:") else f"{dataset}.csv"
-    )
+def compare_data_csv(p1, p2, data=None, dataset=None):
+    if dataset is not None:
+        if dataset.startswith("benchmark:"):
+            fname = f"{dataset[10:]}.csv"
+        else:
+            fname = f"{dataset}.csv"
+    elif data is not None:
+        fname = Path(data).with_suffix(".csv").name
+    else:
+        raise ValueError("Unexpectedly did not receive a value for 'data' nor for 'dataset'.")
+
     assert calc_hash(p1 / "data" / fname) == calc_hash(p2 / "data" / fname)
