78bd109e462d7623aa6528139a9430d136be7632
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-10-30 14:23:58 +0100

    moved some xfail reasons from it to mocked; made the error message generation a bit more compact

--- tests/helpers/_get_xfails_mocked.py
+++ tests/helpers/_get_xfails_mocked.py
@@ -1,17 +1,24 @@
-def _make_msg(fex):
-    return f"asreview.models.feature_extraction.{fex} instance doesn't have a method named 'get_embedding_matrix'"
+msgs = [
+    lambda fex: f"asreview.models.feature_extraction.{fex} instance doesn't have a method named 'get_embedding_matrix'",
+    "'asreview simulate' doesn't use its '--embedding' argument (#34)"
+]
 
 
 def get_xfails_mocked(parameterization):
     reasons = {
-        "doc2vec,lstm-base": _make_msg("Doc2Vec"),
-        "doc2vec,lstm-pool": _make_msg("Doc2Vec"),
-        "embedding-idf,lstm-base": _make_msg("EmbeddingIdf"),
-        "embedding-idf,lstm-pool": _make_msg("EmbeddingIdf"),
-        "sbert,lstm-base": _make_msg("SBERT"),
-        "sbert,lstm-pool": _make_msg("SBERT"),
-        "tfidf,lstm-base": _make_msg("Tfidf"),
-        "tfidf,lstm-pool": _make_msg("Tfidf"),
+        "doc2vec,lstm-base": msgs[0]("Doc2Vec"),
+        "doc2vec,lstm-pool": msgs[0]("Doc2Vec"),
+        "embedding-idf,logistic": msgs[1],
+        "embedding-idf,lstm-base": " and ".join([msgs[1], msgs[0]("EmbeddingIdf")]),
+        "embedding-idf,lstm-pool": " and ".join([msgs[1], msgs[0]("EmbeddingIdf")]),
+        "embedding-idf,nb": msgs[1],
+        "embedding-idf,nn-2-layer": msgs[1],
+        "embedding-idf,rf": msgs[1],
+        "embedding-idf,svm": msgs[1],
+        "sbert,lstm-base": msgs[0]("SBERT"),
+        "sbert,lstm-pool": msgs[0]("SBERT"),
+        "tfidf,lstm-base": msgs[0]("Tfidf"),
+        "tfidf,lstm-pool": msgs[0]("Tfidf"),
     }
     present = parameterization in reasons.keys()
     return present, reasons[parameterization] if present else ""
