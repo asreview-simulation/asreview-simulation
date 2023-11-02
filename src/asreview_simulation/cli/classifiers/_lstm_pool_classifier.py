0fd76fb9dbf8edbc4adaf808ec1feefd3684f51f
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-02 11:58:29 +0100

    reworked the cli for cls-lstm-pool, updated unit test

--- src/asreview_simulation/cli/classifiers/_lstm_pool_classifier.py
+++ src/asreview_simulation/cli/classifiers/_lstm_pool_classifier.py
@@ -15,12 +15,40 @@ name = LSTMPoolClassifier.name
 @click.option(
     "--backwards",
     "backwards",
-    help="hyperparameter",
+    help="Include this flag to have a backward LSTM (default is forward).",
+    is_flag=True
+)
+@click.option(
+    "--batch_size",
+    "batch_size",
+    default=32,
+    help="Size of the batch size for the LSTM model.",
+    show_default=True,
+    type=click.INT,
+)
+@click.option(
+    "--class_weight",
+    "class_weight",
+    default=30.0,
+    help="Class weight for the included papers.",
+    show_default=True,
+    type=click.FLOAT,
 )
 @click.option(
     "--dropout",
     "dropout",
-    help="hyperparameter",
+    default=0.4,
+    help="Value in [0, 1.0) that gives the dropout and recurrent dropout rate for the LSTM model.",
+    show_default=True,
+    type=click.FLOAT,
+)
+@click.option(
+    "--epochs",
+    "epochs",
+    default=35,
+    help="Number of epochs to train the LSTM model.",
+    show_default=True,
+    type=click.INT,
 )
 @click.option(
     "-f",
@@ -30,47 +58,45 @@ name = LSTMPoolClassifier.name
     is_flag=True,
 )
 @click.option(
-    "--optimizer",
-    "optimizer",
-    help="hyperparameter",
+    "--learn_rate",
+    "learn_rate",
+    default=1.0,
+    help="Learn rate multiplier of default learning rate.",
+    show_default=True,
+    type=click.FLOAT,
 )
 @click.option(
     "--lstm_out_width",
     "lstm_out_width",
-    help="hyperparameter",
+    default=20,
+    help="Output width of the LSTM.",
+    show_default=True,
+    type=click.INT,
 )
 @click.option(
     "--lstm_pool_size",
     "lstm_pool_size",
-    help="hyperparameter",
+    default=128,
+    help="Size of the pool, must be a divisor of max_sequence_length.",
+    show_default=True,
+    type=click.INT,
 )
 @click.option(
-    "--learn_rate",
-    "learn_rate",
-    help="hyperparameter",
-)
-@click.option(
-    "--batch_size",
-    "batch_size",
-    help="hyperparameter",
-)
-@click.option(
-    "--epochs",
-    "epochs",
-    help="hyperparameter",
+    "--optimizer",
+    "optimizer",
+    default="rmsprop",
+    help="Optimizer to use.",
+    show_default=True,
+    type=click.Choice(["rmsprop", "sgd", "adagrad", "adam", "nadam"]),
 )
 @click.option(
     "--shuffle",
     "shuffle",
-    help="hyperparameter",
-)
-@click.option(
-    "--class_weight",
-    "class_weight",
-    help="hyperparameter",
+    help="Include this flag to shuffle the data before starting to train.",
+    is_flag=True,
 )
 @click.pass_obj
-def lstm_pool_classifier(obj, backwards, dropout, force, optimizer, lstm_out_width, lstm_pool_size, learn_rate, batch_size, epochs, shuffle, class_weight):
+def lstm_pool_classifier(obj, backwards, batch_size, class_weight, dropout, epochs, force, learn_rate, lstm_out_width, lstm_pool_size, optimizer, shuffle):
     if not force:
         assert obj.provided.classifier is False, (
             "Attempted reassignment of classifier. Use the --force flag "
@@ -79,14 +105,14 @@ def lstm_pool_classifier(obj, backwards, dropout, force, optimizer, lstm_out_wid
     obj.classifier.abbr = name
     obj.classifier.params = {
         "backwards": backwards,
+        "batch_size": batch_size,
+        "class_weight": class_weight,
         "dropout": dropout,
-        "optimizer": optimizer,
+        "epochs": epochs,
+        "learn_rate": learn_rate,
         "lstm_out_width": lstm_out_width,
         "lstm_pool_size": lstm_pool_size,
-        "learn_rate": learn_rate,
-        "batch_size": batch_size,
-        "epochs": epochs,
+        "optimizer": optimizer,
         "shuffle": shuffle,
-        "class_weight": class_weight
     }
     obj.provided.classifier = True
