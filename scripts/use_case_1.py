71b7f3d772d3994c06ff0c9cc2e82305a55b8e68
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-08 14:15:31 +0100

    updated minimal use case of the api

--- scripts/use_case_1.py
+++ scripts/use_case_1.py
@@ -1,5 +1,5 @@
+import os
 from tempfile import TemporaryDirectory
-from pathlib import Path
 from asreview_simulation.api import ModelConfigs
 from asreview_simulation.api import run
 from asreview_simulation.api import prep_project_directory
@@ -10,7 +10,7 @@ def run_use_case():
     models = ModelConfigs()
     benchmark = list_dataset_names()[4]
     with TemporaryDirectory(prefix="asreview-simulation.") as tmpdir:
-        out_file = Path(tmpdir) / "simulate.asreview"
+        out_file = f"{tmpdir}{os.sep}simulate.asreview"
         project, as_data = prep_project_directory(benchmark, input_file=None, output_file=out_file)
         run(models, project, as_data)
         print("done")
