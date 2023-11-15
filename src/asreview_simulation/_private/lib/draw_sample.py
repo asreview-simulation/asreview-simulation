b67b4c2aa742e5b9da8f8e980d6d4b8f0e3f9c34
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 11:38:54 +0100

    added typing to function in public api

--- src/asreview_simulation/_private/lib/draw_sample.py
+++ src/asreview_simulation/_private/lib/draw_sample.py
@@ -2,6 +2,6 @@ import hyperopt
 from asreview_simulation._private.lib.config import PartialConfig
 
 
-def draw_sample(pyll):
+def draw_sample(pyll) -> PartialConfig:
     sample = hyperopt.pyll.stochastic.sample(pyll)
     return PartialConfig(abbr=sample["abbr"], params=sample["params"])
