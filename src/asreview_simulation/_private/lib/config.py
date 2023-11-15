179c79e9d5dc85e30e97317c540919bf5b90b15c
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 11:38:27 +0100

    updated PartialConfig and CompleteConfig definition to avoid circular references

--- src/asreview_simulation/_private/lib/config.py
+++ src/asreview_simulation/_private/lib/config.py
@@ -1,12 +1,19 @@
-from asreview_simulation._private.lib.get_default_config import get_default_config
+from asreview_simulation._private.lib.get_default_params import get_default_params
 
 
 class PartialConfig:
-    def __init__(self, abbr: str, params: dict):
+    def __init__(self, abbr: str, params: dict | None = None):
+        default_params = get_default_params(abbr)
         self.abbr = abbr
-        self.params = params
+        self.params = default_params
+        if params is not None:
+            valid_keys = default_params.keys()
+            provided_keys = params.keys()
+            for key in provided_keys:
+                assert key in valid_keys, f"Can't update parameters for model '{self.abbr}' using unknown key '{key}'."
+                self.params[key] = params[key]
 
-    def asdict(self):
+    def asdict(self) -> dict:
         return {
             "abbr": self.abbr,
             "params": self.params,
@@ -14,15 +21,15 @@ class PartialConfig:
 
 
 class CompleteConfig:
-    def __init__(self, bal=None, cls=None, fex=None, qry=None, sam=None, stp=None):
-        self.bal = bal or get_default_config("bal-double")
-        self.cls = cls or get_default_config("cls-nb")
-        self.fex = fex or get_default_config("fex-tfidf")
-        self.qry = qry or get_default_config("qry-max")
-        self.sam = sam or get_default_config("sam-random")
-        self.stp = stp or get_default_config("stp-rel")
+    def __init__(self, bal: PartialConfig = None, cls: PartialConfig = None, fex: PartialConfig = None, qry: PartialConfig = None, sam: PartialConfig = None, stp: PartialConfig = None):
+        self.bal = bal or PartialConfig("bal-double")
+        self.cls = cls or PartialConfig("cls-nb")
+        self.fex = fex or PartialConfig("fex-tfidf")
+        self.qry = qry or PartialConfig("qry-max")
+        self.sam = sam or PartialConfig("sam-random")
+        self.stp = stp or PartialConfig("stp-rel")
 
-    def asdict(self):
+    def asdict(self) -> dict:
         return {
             "bal": self.bal.asdict(),
             "cls": self.cls.asdict(),
