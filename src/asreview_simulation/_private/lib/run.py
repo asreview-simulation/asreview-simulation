8564d0139174527be1d17b8cbe79471cdfa38ff9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 11:39:29 +0100

    added typing to function in public api

--- src/asreview_simulation/_private/lib/run.py
+++ src/asreview_simulation/_private/lib/run.py
@@ -7,7 +7,7 @@ from asreview_simulation._private.lib.unwrapping.get_review_simulate_kwargs impo
 
 def run(
     models: CompleteConfig, project: ASReviewProject, as_data: ASReviewData, write_interval: int = None, seed: int = None
-):
+) -> None:
     # prep
     kwargs = get_review_simulate_kwargs(models, as_data, seed)
     reviewer = ReviewSimulate(as_data, project=project, **kwargs, write_interval=write_interval)
