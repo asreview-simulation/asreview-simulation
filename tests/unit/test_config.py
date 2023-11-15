e2ac3c6cf026ed3a8e63f4f07d0d4fbfd4ba04f4
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 17:34:09 +0100

    added descriptions to PartialConfig unit tests to explain their intent

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -6,20 +6,34 @@ from asreview_simulation.api import PartialConfig
 
 @pytest.mark.parametrize("abbr", get_abbrs())
 def test_construct_partial_config_from_positional_args_abbr_only(abbr):
+    # constructing from abbr only should yield the default parameter values
     actual = PartialConfig(abbr)
     expected = PartialConfig(abbr=abbr, params=get_default_params(abbr))
     assert actual == expected
 
 
 @pytest.mark.parametrize("abbr", get_abbrs())
-def test_construct_partial_config_from_positional_args(abbr):
-    actual = PartialConfig(abbr, get_default_params(abbr))
-    expected = PartialConfig(abbr=abbr, params=get_default_params(abbr))
-    assert actual == expected
+def test_construct_partial_config_from_positional_arg_partial_params(abbr):
+    # constructing from abbr and partial params should yield a PartialConfig
+    # instance with the default parameter values given the model + changes
+    # from the pair passed to the constructor
+    params = get_default_params(abbr)
+    if len(params.keys()) >= 1:
+        key, _ = list(params.items())[0]
+        one_pair = {key: "test value"}
+        actual = PartialConfig(abbr, one_pair).params
+        expected = params.copy()
+        expected.update(one_pair)
+        assert actual == expected
+    else:
+        pytest.skip(reason=f"{abbr} has no parameters to test")
 
 
 @pytest.mark.parametrize("abbr", get_abbrs())
-def test_construct_partial_config_from_partial_kwargs(abbr):
+def test_construct_partial_config_from_kwargs_partial_params(abbr):
+    # constructing from abbr and partial params kwargs should yield a PartialConfig
+    # instance with the default parameter values given the model + changes
+    # from the pair passed to the constructor
     params = get_default_params(abbr)
     if len(params.keys()) >= 1:
         key, _ = list(params.items())[0]
@@ -32,24 +46,31 @@ def test_construct_partial_config_from_partial_kwargs(abbr):
         pytest.skip(reason=f"{abbr} has no parameters to test")
 
 
+def test_raising_construct_partial_config_nonexistent_model():
+    # constructing from an unknown abbr should raise an AsssertionError
+    with pytest.raises(KeyError):
+        PartialConfig(abbr="unknown-abbr")
+
+
 @pytest.mark.parametrize("abbr", get_abbrs())
 def test_raising_construct_partial_config_from_kwargs_with_extras(abbr):
+    # constructing from abbr and params using kwargs with an unknown key
+    # should raise an AsssertionError
     with pytest.raises(AssertionError):
         params = get_default_params(abbr)
-        params.update({"xyz123456": "extra key"})
+        params.update({"unknown-parameter": "value"})
         PartialConfig(abbr=abbr, params=params)
 
 
-def test_raising_construct_partial_config_nonexistent_model():
-    with pytest.raises(KeyError):
-        PartialConfig(abbr="xyz")
-
-
 def test_raising_construct_partial_config_from_positional_args_abbr_only_wrong_type():
+    # constructing from position argument abbr of the wrong
+    # type should raise an AsssertionError
     with pytest.raises(AssertionError):
         PartialConfig({"wrong": "type"})
 
 
 def test_raising_construct_partial_config_from_positional_args_wrong_type():
+    # constructing from position argument params of the wrong
+    # type should raise an AsssertionError
     with pytest.raises(AssertionError):
         PartialConfig("bal-double", 12345)
