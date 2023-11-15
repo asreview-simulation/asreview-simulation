d5916943db9879039ac7203043c5be8f9cbcc658
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 18:28:38 +0100

    updated parameterization from pyll test to be more specific in the expected result type

--- tests/unit/test_parameterizing_models_from_pyll.py
+++ tests/unit/test_parameterizing_models_from_pyll.py
@@ -1,39 +1,25 @@
 import pytest
-from asreview.models.base import BaseModel as ASReviewBaseModel
+from asreview.models.balance.base import BaseModel as ASReviewBaseModelBal
+from asreview.models.classifiers.base import BaseModel as ASReviewBaseModelCls
+from asreview.models.feature_extraction.base import BaseModel as ASReviewBaseModelFex
+from asreview.models.query.base import BaseModel as ASReviewBaseModelQry
+from asreview_simulation.api import get_abbrs
 from asreview_simulation.api import draw_sample
 from asreview_simulation.api import get_pyll
 from asreview_simulation.api.unwrapping import instantiate_unwrapped_model
 
 
-def get_abbrs():
-    return [
-        "bal-double",
-        "bal-simple",
-        "bal-undersample",
-        "cls-logistic",
-        "cls-lstm-base",
-        "cls-lstm-pool",
-        "cls-nb",
-        "cls-nn-2-layer",
-        "cls-rf",
-        "cls-svm",
-        "fex-doc2vec",
-        "fex-embedding-idf",
-        "fex-embedding-lstm",
-        "fex-sbert",
-        "fex-tfidf",
-        "qry-cluster",
-        "qry-max",
-        "qry-max-random",
-        "qry-max-uncertainty",
-        "qry-random",
-        "qry-uncertainty",
-    ]
-
-
 @pytest.mark.parametrize("abbr", get_abbrs())
 def test_parameterizing_models_from_pyll(abbr):
+    if abbr[:3] in {"sam", "stp"}:
+        pytest.skip(reason=f"ASReview doesn't have an equivalent model for models of type {abbr}")
     pyll = get_pyll(abbr)
     model = draw_sample(pyll)
     model_unwrapped = instantiate_unwrapped_model(model, random_state=12345)
+    ASReviewBaseModel = {
+        "bal": ASReviewBaseModelBal,
+        "cls": ASReviewBaseModelCls,
+        "fex": ASReviewBaseModelFex,
+        "qry": ASReviewBaseModelQry,
+    }[abbr[:3]]
     assert isinstance(model_unwrapped, ASReviewBaseModel)
