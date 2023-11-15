2f9af3e59c9086883383ae800514a790a73aacd1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 18:29:50 +0100

    fiddling with the packaging of the data returned by draw_sample

--- src/asreview_simulation/_private/lib/draw_sample.py
+++ src/asreview_simulation/_private/lib/draw_sample.py
@@ -1,5 +1,7 @@
 import hyperopt
+from asreview_simulation.api import PartialConfig
 
 
 def draw_sample(pyll):
-    return hyperopt.rand.pyll.stochastic.sample(pyll)
+    sample = hyperopt.rand.pyll.stochastic.sample(pyll)
+    return PartialConfig(abbr=sample["abbr"], params=sample["params"])
