f551466af16071a1330b3bf0cf0e61f1612e2dac
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-15 11:27:51 +0100

    uopdated the uise case to use recently-changed config objects

--- scripts/use_case_1.py
+++ scripts/use_case_1.py
@@ -1,13 +1,26 @@
 import os
 from tempfile import TemporaryDirectory
 from asreview_simulation.api import list_dataset_names
-from asreview_simulation.api import ModelConfigs
+from asreview_simulation.api import CompleteConfig
+from asreview_simulation.api import PartialConfig
 from asreview_simulation.api import prep_project_directory
 from asreview_simulation.api import run
 
 
 def run_use_case():
-    models = ModelConfigs()
+    # make partial config using default parameter values given the model name
+    cls = PartialConfig("cls-svm")
+
+    # make partial config using positional arguments, partial params dict
+    qry = PartialConfig("qry-max-random", {"fraction_max": 0.90})
+
+    # make partial config using keyword arguments
+    stp = PartialConfig(abbr="stp-nq", params={"n_queries": 10})
+
+    # construct a complete config from partial configs -- implicitly use default model choice
+    # and parameterization for models not included as argument (i.e. bal, stp, sam)
+    models = CompleteConfig(cls=cls, qry=qry, stp=stp)
+
     benchmark = list_dataset_names()[4]
     with TemporaryDirectory(prefix="asreview-simulation.") as tmpdir:
         out_file = f"{tmpdir}{os.sep}simulate.asreview"
