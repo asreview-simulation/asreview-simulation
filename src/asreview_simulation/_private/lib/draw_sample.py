e136444b12bc34508b0baa50e21e3b512bb331df
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-16 16:38:15 +0100

    updated draw_pyll to return parameterization that can be passed to CompleteConfig

--- src/asreview_simulation/_private/lib/draw_sample.py
+++ src/asreview_simulation/_private/lib/draw_sample.py
@@ -2,6 +2,19 @@ import hyperopt
 from asreview_simulation.api import PartialConfig
 
 
-def draw_sample(pyll):
+def draw_sample(pyll) -> dict[str, PartialConfig]:
+    valid_keys = {"bal", "cls", "fex", "qry", "sam", "stp"}
+    assert type(pyll) == dict, "Expected input argument pyll to be of type 'dict'."
+    for key in pyll.keys():
+        assert key in valid_keys, f"Unexpected key '{key}' in keys of input argument 'pyll'."
     sample = hyperopt.rand.pyll.stochastic.sample(pyll)
-    return PartialConfig(abbr=sample["abbr"], params=sample["params"])
+    d = {}
+    for key in sample.keys():
+        abbr = sample[key]["abbr"]
+        params = sample[key]["params"]
+        pair = {
+            key: PartialConfig(abbr=abbr, params=params)
+        }
+        d.update(pair)
+    return d
+
