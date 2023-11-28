81fcec446d40464063a1f5674ff436537e4080a8
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-11-28 19:42:56 +0100

    added test for AllModelConfig flattening

---
+++ tests/unit/test_all_model_config.py
@@ -0,0 +1,23 @@
+from asreviewcontrib.simulation.api import AllModelConfig
+
+
+def test_flattened():
+    models = AllModelConfig()
+    actual = models.flattened()
+    assert isinstance(actual, dict)
+    expected = {
+        "bal-double/a": 2.155,
+        "bal-double/alpha": 0.94,
+        "bal-double/b": 0.789,
+        "bal-double/beta": 1.0,
+        "cls-nb/alpha": 3.822,
+        "fex-tfidf/ngram_max": 1,
+        "fex-tfidf/split_ta": False,
+        "fex-tfidf/stop_words": "english",
+        "fex-tfidf/use_keywords": False,
+        "qry-max/n_instances": 1,
+        "sam-random/init_seed": None,
+        "sam-random/n_excluded": 1,
+        "sam-random/n_included": 1
+    }
+    assert actual == expected
