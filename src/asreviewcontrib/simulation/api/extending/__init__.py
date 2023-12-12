d9ed215c88c68ecd1741efc6289acc21173453cc
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-12 13:10:49 +0100

    added helper class for extending via plugins to public api

--- src/asreviewcontrib/simulation/api/extending/__init__.py
+++ src/asreviewcontrib/simulation/api/extending/__init__.py
@@ -6,9 +6,11 @@ from asreviewcontrib.simulation._private.cli.cli_msgs import dont_reassign_ofn_m
 from asreviewcontrib.simulation._private.cli.cli_msgs import dont_reassign_qry_msg
 from asreviewcontrib.simulation._private.cli.cli_msgs import dont_reassign_sam_msg
 from asreviewcontrib.simulation._private.cli.cli_msgs import dont_reassign_stp_msg
+from asreviewcontrib.simulation._private.lib.extending.plugin_quad import PluginQuad
 
 
 __all__ = [
+    "PluginQuad",
     "dont_reassign_bal_msg",
     "dont_reassign_clr_msg",
     "dont_reassign_fex_msg",
