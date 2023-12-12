7594b7c4b9c0c3368fbb5a79557a977f46ab049a
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-12 13:07:29 +0100

    can now load ofn from plugin

--- src/asreviewcontrib/simulation/_private/lib/calc_ofn_score.py
+++ src/asreviewcontrib/simulation/_private/lib/calc_ofn_score.py
@@ -1,11 +1,17 @@
 from asreviewcontrib.simulation._private.lib.ofn.ofn_none_fun import ofn_none_fun
 from asreviewcontrib.simulation._private.lib.ofn.ofn_wss_fun import ofn_wss_fun
+from asreviewcontrib.simulation._private.lib.get_quads import get_quads
 
 
 def calc_ofn_score(ofn, project_path):
-    return {
+    my_ofns = {
         "ofn-none": ofn_none_fun,
         "ofn-wss": ofn_wss_fun,
-    }[
-        ofn.abbr
-    ](project_path, **ofn.params)
+    }
+    other_ofns = [{name: q.impl} for name, q in get_quads() if name.startswith("ofn")]
+
+    ofns = my_ofns
+    for other_ofn in other_ofns:
+        ofns.update(other_ofn)
+
+    return ofns[ofn.abbr](project_path, **ofn.params)
