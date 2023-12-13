e1088cb1ab6c969ef0a6cc9f8a8233ac803510cb
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-13 13:31:50 +0100

    simplified unwrapping of ReviewSimulate kwargs

--- src/asreviewcontrib/simulation/_private/lib/qry/qry_cluster_unwrap.py
+++ src/asreviewcontrib/simulation/_private/lib/qry/qry_cluster_unwrap.py
@@ -6,4 +6,10 @@ def instantiate_unwrapped_qry_cluster(params, random_state):
         "cluster_size": params["cluster_size"],
         "update_interval": params["update_interval"],
     }
+
+    # asreview's query model does not expect n_instances as part
+    # of the params dict but as a separate variable
+    assert "n_instances" in params.keys()
+    assert "n_instances" not in mapped_params.keys()
+
     return ClusterQuery(**mapped_params, random_state=random_state)
--- src/asreviewcontrib/simulation/_private/lib/qry/qry_max_random_unwrap.py
+++ src/asreviewcontrib/simulation/_private/lib/qry/qry_max_random_unwrap.py
@@ -7,4 +7,10 @@ def instantiate_unwrapped_qry_max_random(params, random_state):
         "strategy_2": "random",
         "mix_ratio": params["fraction_max"],
     }
+
+    # asreview's query model does not expect n_instances as part
+    # of the params dict but as a separate variable
+    assert "n_instances" in params.keys()
+    assert "n_instances" not in mapped_params.keys()
+
     return MixedQuery(**mapped_params, random_state=random_state)
