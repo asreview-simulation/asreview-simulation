c3f15c1c75be69c1a282a882ab709d93f6356b48
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-18 17:42:30 +0100

    renamed parameter init_seed to just seed, updated references

--- src/asreviewcontrib/simulation/_private/lib/sam/sam_random_pyll.py
+++ src/asreviewcontrib/simulation/_private/lib/sam/sam_random_pyll.py
@@ -7,6 +7,6 @@ def sam_random_pyll():
         "params": {
             "n_excluded": hyperopt.hp.choice("n_excluded", [1]),  # TODO
             "n_included": hyperopt.hp.choice("n_included", [1]),  # TODO
-            "seed": hyperopt.hp.choice("init_seed", [None]),  # TODO
+            "seed": hyperopt.hp.choice("seed", [None]),  # TODO
         },
     }
