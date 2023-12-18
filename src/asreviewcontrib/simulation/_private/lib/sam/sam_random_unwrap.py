f02652e0c9ab1bf5d2d57dc5e2bb829620c8d221
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-12-18 15:46:25 +0100

    fixing mypy error

--- src/asreviewcontrib/simulation/_private/lib/sam/sam_random_unwrap.py
+++ src/asreviewcontrib/simulation/_private/lib/sam/sam_random_unwrap.py
@@ -9,7 +9,7 @@ TSamResult = Tuple[List[int], int, int, Optional[int]]
 
 
 def sam_random_unwrap(config: Config, _as_data: ASReviewData) -> TSamResult:
-    prior_indices = list()
+    prior_indices: List[int] = list()
     n_prior_included = config.sam.params["n_included"]
     n_prior_excluded = config.sam.params["n_excluded"]
     init_seed = config.sam.params["init_seed"]
